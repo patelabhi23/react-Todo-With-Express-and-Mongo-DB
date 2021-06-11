54d16d57831c8124634788430bc1a36cf7c9239c
patelabhi23 <patelabhi2301@gmail.com>
2021-06-11 18:57:04 +0530

    completeProject

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import todos from "./apis";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Form from "./components/Form";
+import Section from "./components/Section";
+import List from "./components/List";
+
+const appTitle = "To-Do App";
+
+const App = () => {
+    const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await todos.get("/todos");
+            setTodoList(data);
+        }
+
+        fetchData();
+    }, []);
+
+    const addTodo = async (item) => {
+        const { data } = await todos.post("/todos", item);
+        setTodoList((oldList) => [...oldList, data]);
+    };
+
+    const removeTodo = async (id) => {
+        await todos.delete(`/todos/${id}`);
+        setTodoList((oldList) => oldList.filter((item) => item._id !== id));
+    };
+
+    const editTodo = async (id, item) => {
+        await todos.put(`/todos/${id}`, item);
+    };
+
+    return (
+        <div className="ui container center aligned">
+            <Section>
+                <h1>{appTitle}</h1>
+            </Section>
+
+            <Section>
+                <Form addTodo={addTodo} />
+            </Section>
+
+            <Section>
+                <List
+                    editTodoListProp={editTodo}
+                    removeTodoListProp={removeTodo}
+                    list={todoList}
+                />
+            </Section>
+        </div>
+    );
+};
 
 export default App;
